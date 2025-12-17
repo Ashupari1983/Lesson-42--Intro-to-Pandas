9ad6880e7adc2c4524ea92e4e9e83ec969cf9d6d
Ashupari1983 <ashupari123@gmail.com>
2025-12-17 18:27:58 +0530

    Intro to Pandas

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("My name is Navika. I'm using a Macbook.")
+import pandas as pd
+import numpy as np
+
+exam_data = {"Name":["Belle", "Anne", "Arora", "Michael", "Kris"],
+             "Score":[9, np.nan, 9.5, 10, np.nan],
+             "Attempts":[1, 1, 2, 2, 1],
+             "Grade":[10, 10, 10, 10, 10],
+             "Qualify":["yes","yes","yes","yes","yes"]}
+
+labels = ["a", "b", "c", "d", "e"]
+
+df = pd.DataFrame(exam_data, index = labels)
+print(df.head())
+print(df.info())
